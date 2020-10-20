8c37f2961384cead0f585e1a2516cedf7d23598b
Miftahul Lubab <miftahul.lubab20@gmail.com>
2020-10-20 22:17:33 +0700

    :tada: Initial Project

---
+++ deploy.php
@@ -0,0 +1,43 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'laravel-deployer');
+
+// Project repository
+set('repository', 'https://github.com/babul28/laravel-deployer.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('139.59.99.5')
+	->user('deployer')
+    	->identityFile('~/.ssh/deployer')
+    	->set('deploy_path', '/var/www/html/api-mangdropship');
+   
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
